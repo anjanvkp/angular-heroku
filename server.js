b293b93979bd2a7ed5d537f7bc52afbd7799f150
anjanvkp <anjanvkp@gmail.com>
2020-05-13 16:47:24 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files....
+app.use(express.static(__dirname + '/dist/anjn-angular'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/anjn-angular/index.html'));
+});
+
+// default Heroku PORT
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
